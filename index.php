21d83f22f9a1aafe8b9223e2ca1e59b8f0009305
NaSuRal <romain.poulain@edu.esiee-it.fr>
2024-11-11 17:22:04 +0100

    First Version

---
+++ index.php
@@ -0,0 +1,37 @@
+<?php
+?>
+
+
+<!doctype html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="./css/style.css">
+    <title>Portfolio</title>
+</head>
+<body>
+
+
+<div class="container">
+    <div class="titre">
+        <h1>Project: Nasu</h1>
+        <h2>Portfolio Romain Poulain...</h2>
+    </div>
+    <div class="texte">
+        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dolor ex fugit laboriosam nobis quod sunt? Aliquam doloremque dolores inventore, magni officia praesentium quas temporibus tenetur? Ad earum magnam qui sapiente.</p>
+    </div>
+
+    <form id="commandForm" class="input">
+        <span1>root/</span1><span> $</span>
+        <input type="text" id="commandInput" autocomplete="off">
+
+    </form>
+
+</div>
+
+
+</body>
+</html>
